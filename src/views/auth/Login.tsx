2937a79f2273ab56a795dae078fa936eb5ee054c
Shohruh <shohruhllj@gmail.com>
2026-08-11 10:28:02 +0500

    added new numbers

--- src/views/auth/Login.tsx
+++ src/views/auth/Login.tsx
@@ -5,9 +5,10 @@ const Login = () => {
 			<div className='max-w-xl w-full flex border bg-white/30 backdrop-blur-md rounded-lg flex-col justify-center items-center px-10 py-4'>
 				<img className='w-48 h-48 bg-white rounded-full mb-2' src="/logo.png" alt="" />
 				<h1 className='text-4xl mb-5 text-white'>Tizimga kirish</h1>
-				<div className='flex justify-between gap-4 items-center mb-5'>
+				<div className='flex flex-col gap-1 items-center mb-5'>
 					<p className='text-white max-md:text-sms'>Murojaat uchun:</p>
-					<a href='tel:+998975852404' className='text-white flex items-center max-md:text-xs'>+998 97 585 24 04</a>
+					<a href='tel:+998970098811' className='text-white flex items-center gap-2 max-md:text-xs'>Shaxzod: +998 97 009 88 11</a>
+					<a href='tel:+998975852404' className='text-white flex items-center gap-2 max-md:text-xs'>Begzod: +998 97 585 24 04</a>
 			    </div>
 				<LoginForm />
 			</div>
